bb77f9b0f9f39c2e848975facda5b46e5e6c3880
Mathieu Voyer <mathieu.voyer@etu.unistra.fr>
2022-01-20 21:24:59 +0100

    better format and args use, added ping & emote

--- src/bot.js
+++ src/bot.js
@@ -24,8 +24,17 @@ client.on('message', async (message) => {
     const [CMD_NAME, ...args] = message.content
       .trim()
       .substring(PREFIX.length)
-      .split(/\s+/);
-    if (CMD_NAME === 'qui_vient') {
+      .split(/ +/g);
+
+
+    if (CMD_NAME === 'ping') {
+      message.reply(`Pong ` + args);
+    }
+    if (CMD_NAME === 'emote') {
+      message.reply("\:kongstrong:");
+    }
+    
+    if (CMD_NAME === 'soiree') {
       const hasRole = message.member.roles.cache.filter(role => role.id === process.env.BO_ID 
         || role.ID === process.env.MOITIE_BO_ID 
         || role.ID === process.env.JOKER_BO_ID
@@ -39,11 +48,10 @@ client.on('message', async (message) => {
 
       const channel = client.channels.cache.find(channel => channel.id === message.channel.id)
       const channelWebHook = client.channels.cache.find(channel => channel.id === process.env.WEBHOOK_CHANNEL_ID)
+      
 
       try {
-        const date = args.join(" ").split('date: ')[1].split('heure: ')[0];
-        const heure = args.join(" ").split('heure: ')[1].split('lieu: ')[0];
-        const lieu = args.join(" ").split('lieu: ')[1];
+        let [date, heure, lieu] = args;
         
         console.log("Date: " + date);
         console.log("Heure: " + heure);
@@ -51,26 +59,21 @@ client.on('message', async (message) => {
 
 
         if (date === null || heure === null || lieu === null) {
-          console.log("Un champs est null")
-          return message.reply("Mauvais format .qui_vient date: <date> heure: <heure> lieu: <lieu>");
+          console.log("Un champ est null")
+          return message.reply("Mauvais format : .soiree <date> <heure> <lieu>");
         }
         const linkBO = 'https://cdn.discordapp.com/attachments/681533018351075491/783974280273985557/avatars-000307014030-id36vv-t500x500.jpg'
         const MessageEmbed = new Discord.MessageEmbed()
-        .setColor('#0099ff')
-	      .setTitle('Bon qui vient !')
-        .setAuthor(message.author.tag, message.author.avatarURL())
+        .setColor('#6b00b3')
+	      .setTitle("Une sortie s'organise :                                                                     \u200B")
         .addFields(
-          { name: '\u200B', value: '\u200B' },
-          { name: 'Date', value: date, inline: true },
-		      { name: 'Heure', value: heure, inline: true },
-          { name: 'Lieu', value: lieu},
-          { name: '\u200B', value: '\u200B' },
-          { name: '\u200B', value: '<@&'+process.env.BO_ID+'>' + ' <@&'+process.env.MOITIE_BO_ID+'>' + ' <@&'+process.env.JOKER_BO_ID+'>' + ' <@&'+process.env.BOOMER_BO_ID+'>'}
+          { name: '\u200B', value: '\u200B'},
+          { name: date, value: '\u200B', inline: true }, { name: heure, value: '\u200B', inline: true }, { name: lieu, value: '\u200B', inline: true},
         )
-        .setThumbnail(linkBO)
-        .setFooter('Sortie',linkBO);
+        .setFooter(message.author.tag, message.author.avatarURL());
+        //.setFooter('Sortie',linkBO);
       
-        webhookClient.send('<@&'+process.env.BO_ID+'>' + ' <@&'+process.env.MOITIE_BO_ID+'>' + ' <@&'+process.env.JOKER_BO_ID+'>')
+        //webhookClient.send('<@&'+process.env.BO_ID+'>' + ' <@&'+process.env.MOITIE_BO_ID+'>' + ' <@&'+process.env.JOKER_BO_ID+'>')
         webhookClient.send(MessageEmbed)
           .then((message) => {
             channelWebHook.messages.fetch().then((messages) => {
@@ -78,6 +81,7 @@ client.on('message', async (message) => {
               webHookMsgReact.react("✅");
               webHookMsgReact.react("❔");
               webHookMsgReact.react("❌");
+              webHookMsgReact.react('<a:kongstrong:933816635809210368>');
             })
           })
 
@@ -88,19 +92,11 @@ client.on('message', async (message) => {
       }
       } catch (error) {
         console.log("Erreur send:\n" + error)
-        return message.reply("Mauvais format .qui_vient date: <date> heure: <heure> lieu: <lieu>");
+        return message.reply("Mauvais format .soiree <date> <heure> <lieu>");
       }
-
-
-
-
-
 
     }
   }
 });
-
-
-
 
 client.login(process.env.BOT_TOKEN);
